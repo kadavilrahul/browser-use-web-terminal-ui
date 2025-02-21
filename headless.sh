beb13e977c7d2b7d223a6caedafc758c0d54569c
Rahul Dinesh <94444265+kadavilrahul@users.noreply.github.com>
2025-02-21 12:26:28 +0530

    Update headless.sh

--- headless.sh
+++ headless.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Update package list and install xvfb
+sudo apt-get update
+sudo apt-get install -y xvfb
+
+
 # Kill any process using port 7860
 kill -9 $(lsof -t -i :7860) 2>/dev/null
 
 # Activate the virtual environment
 source venv/bin/activate
 
-# Run the main Python script on playwright headless mode
-xvfb-run python3 main.py
+# Run the main Python script
+xvfb-run -a python3 main.py
